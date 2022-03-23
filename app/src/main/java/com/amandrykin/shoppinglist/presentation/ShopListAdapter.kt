106f3c40b6fd299ba354f5db222f510e6c042d75
amandrykin <amandrykin@yandex.ru>
2022-03-23 14:53:07 +0300

    bugs when using RecyclerView

--- app/src/main/java/com/amandrykin/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/amandrykin/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,30 +4,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.amandrykin.shoppinglist.R
 import com.amandrykin.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disable, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disable,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
-        viewHolder.tvName.text = shopItem.name
-        viewHolder.tvCount.text = shopItem.count.toString()
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled) {
+            "Active"
+        } else {
+            "Not active"
+        }
         viewHolder.view.setOnLongClickListener {
             true
         }
+        if (shopItem.enabled) {
+            viewHolder.tvName.text = "${shopItem.name} $status"
+            viewHolder.tvCount.text = shopItem.count.toString()
+            viewHolder.tvName.setTextColor(
+                ContextCompat.getColor
+                    (viewHolder.view.context, android.R.color.holo_red_light)
+            )
+        }
+
+// 1-й способ добавить блок else
+//        else {
+//            viewHolder.tvName.text = ""
+//            viewHolder.tvCount.text = ""
+//            viewHolder.tvName.setTextColor(ContextCompat.getColor
+//                (viewHolder.view.context, android.R.color.white))
+//        }
+    }
+
+// 2-й способ переопределить метод onViewRecycled
+    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
+        super.onViewRecycled(viewHolder)
+        viewHolder.tvName.text = ""
+        viewHolder.tvCount.text = ""
+        viewHolder.tvName.setTextColor(
+            ContextCompat.getColor
+                (viewHolder.view.context, android.R.color.white)
+        )
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
